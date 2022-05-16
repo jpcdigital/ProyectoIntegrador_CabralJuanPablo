77f73f4ff10a0c56bdbec2794fae6f0879417743
Juan Pablo Cabral <cabraljuanpablo@gmail.com>
2022-05-16 17:07:08 -0300

    Creación Carpeta BackEnd

---
+++ ProyectoIntegrador_CabralJuanPablo/BackEnd/src/main/java/com/portfolio/jpc/JpcApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpcApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_CabralJuanPablo/BackEnd/src/test/java/com/portfolio/jpc/JpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
