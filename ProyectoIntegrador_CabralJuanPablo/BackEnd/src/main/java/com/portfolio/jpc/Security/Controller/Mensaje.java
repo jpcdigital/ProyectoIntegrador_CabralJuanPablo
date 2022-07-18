86204d2e7fea80e72edf8ade98cc80e7b1f1e9d7
jpcdigital <cabraljuanpablo@gmail.com>
2022-07-18 16:14:16 -0300

    varios

---
+++ ProyectoIntegrador_CabralJuanPablo/BackEnd/src/main/java/com/portfolio/jpc/Security/Controller/Mensaje.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.jpc.Security.Controller;
+
+/**
+ *
+ * @author Juan
+ */
+public class Mensaje {
+   private String mensaje;
+   
+   //constructor
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+   //Getter y Setter
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
