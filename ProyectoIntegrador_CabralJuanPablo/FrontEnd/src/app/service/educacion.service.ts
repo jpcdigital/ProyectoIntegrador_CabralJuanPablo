2ca78bcc6728db442230be8538a2903e59e4ef61
jpcdigital <cabraljuanpablo@gmail.com>
2022-08-09 16:44:37 -0300

    casi casi

--- ProyectoIntegrador_CabralJuanPablo/FrontEnd/src/app/service/educacion.service.ts
+++ ProyectoIntegrador_CabralJuanPablo/FrontEnd/src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'http://localhost:8080/educacion/';
+  URL = 'https://portfoliojpcbackend.herokuapp.com/educacion/';
   
   //URL PARA HEROKU
   //URL = 'https://heroku-backendjpc.herokuapp.com/educacion/';
